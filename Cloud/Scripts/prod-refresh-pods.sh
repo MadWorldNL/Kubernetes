b81796e91465a8e321b41befa56ede4b930853ed
Oscar Veldman <oveldman@gmail.com>
2024-03-08 09:33:11 +0100

    Update refresh pods

--- Cloud/Scripts/prod-refresh-pods.sh
+++ Cloud/Scripts/prod-refresh-pods.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo microk8s kubectl -n default rollout restart deploy
-sudo microk8s kubectl -n MadWorldNL rollout restart deploy
+sudo microk8s kubectl -n madworldnl rollout restart deploy
 
 # When new migration is added, then run the following command
-sudo microk8s kubectl rollout restart deployment madworld-identity-grpc-deployment -n MadWorldNL
+# sudo microk8s kubectl rollout restart deployment madworld-identity-grpc-deployment -n madworldnl
